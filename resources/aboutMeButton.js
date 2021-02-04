08330cf8df3f2190b7fc42ffa64fe28f34488b1a
krisbucz <krystianb125@gmail.com>
2021-02-04 20:42:48 +0000

    add media to make a responsive design

--- resources/aboutMeButton.js
+++ resources/aboutMeButton.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $("#aboutMeButton").mouseover(function(){
       $("#aboutMeButton").animate({
-        padding: "40px",
+        padding: "20px",
       });
     });
   });
   $(document).ready(function(){
     $("#aboutMeButton").mouseout(function(){
       $("#aboutMeButton").animate({
-        padding: "20px",
+        padding: "10px",
       });
     });
   });
